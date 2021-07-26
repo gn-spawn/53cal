615c0ab48e4fe28b4a2e4c01de48448ad9c29896
gn-spawn <gandam00.transam@gmail.com>
2021-07-26 18:13:34 +0900

    サンプル追加

--- example/sample.js
+++ example/sample.js
@@ -1,3 +1,8 @@
 var GomiCal = require("53cal").default;
-var scrape = new GomiCal({ city: 1111, area: 2222 });
-console.log(scrape);
+var scrape = new GomiCal({ city: 1080104, area: 1080104102 });
+
+scrape.whatDate("2021-7-26").then((data) => {
+  console.log(data);
+});
+
+scrape.dateAndCategoryInMonth(2021, 7).then((data) => console.log(data));
